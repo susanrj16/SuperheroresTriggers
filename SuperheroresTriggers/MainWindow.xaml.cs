d1d64243a12a216117dccae1582d532495d95363
SusanRojas <sf.rojas16@iesdoctorbalmis.com>
2020-12-20 02:03:38 +0100

    Agregar archivos de proyecto.

---
+++ SuperheroresTriggers/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+
+
+namespace Superheroes
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Superheroe superheroe;
+        List<Superheroe> lista = Superheroe.GetSamples();
+        int pos = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            heroesDock.DataContext = lista;
+            pagActualTextBlock.Text = "1";
+            pagTotalTextBlock.Text = lista.Count.ToString();
+
+            //Nuevo
+            superheroe = new Superheroe();           
+            nuevoHeroeGrid.DataContext = superheroe;
+        }
+
+
+        public void Avanzar_MouseUp(object sender, MouseEventArgs e)
+        {
+            if(pos+1 < lista.Count)
+            {
+                pos++;
+                heroesDock.DataContext = lista[pos];
+                pagActualTextBlock.Text = Convert.ToString(pos + 1);
+            }
+        }
+
+
+        private void Avanzar_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (pos + 1 < lista.Count)
+            {
+                pos++;
+                heroesDock.DataContext = lista[pos];
+                pagActualTextBlock.Text = Convert.ToString(pos + 1);
+            }
+        }
+
+        private void Atras_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if(pos >= 1)
+            {
+                pos--;
+                heroesDock.DataContext = lista[pos];
+                pagActualTextBlock.Text = Convert.ToString(pos+1);
+                
+            }
+        }
+
+        private void aceptarBoton_Click(object sender, RoutedEventArgs e)
+        {
+            lista.Add(superheroe);
+            pagTotalTextBlock.Text = lista.Count.ToString();
+            MessageBox.Show("Superhéroe insertado con éxito","Superhéroes",MessageBoxButton.OK,MessageBoxImage.Information);
+            
+            superheroe = new Superheroe();
+            nuevoHeroeGrid.DataContext = superheroe;
+
+        }
+
+        private void limpiarBoton_Click(object sender, RoutedEventArgs e)
+        {
+            superheroe.Nombre = "";
+            superheroe.Imagen = "";
+            superheroe.Heroe = true;
+            superheroe.Villano = false;
+            superheroe.Xmen = false;
+            superheroe.Vengador = false;
+        }
+    }
+}
---
+++ SuperheroresTriggers/Superheroe.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+
+namespace Superheroes
+{
+    class Superheroe:INotifyPropertyChanged
+    {
+        private String _nombre;
+
+        public String Nombre
+        {
+            get { return _nombre; }
+            set { 
+                _nombre = value;
+                NotifyPropertyChanged("Nombre");
+            }
+        }
+
+        private String _imagen;
+
+        public String Imagen
+        {
+            get { return _imagen; }
+            set { 
+                _imagen = value;
+                NotifyPropertyChanged("Imagen");
+            }
+        }
+
+        private bool _vengador;
+
+        public bool Vengador
+        {
+            get { return _vengador; }
+            set { 
+                _vengador = value;
+                NotifyPropertyChanged("Vengador");
+            }
+        }
+
+        private bool _xmen;
+
+        public bool Xmen
+        {
+            get { return _xmen; }
+            set { 
+                _xmen = value;
+                NotifyPropertyChanged("Xmen");
+            }
+        }
+
+        private bool _heroe;
+
+        public bool Heroe
+        {
+            get { return _heroe; }
+            set { 
+                _heroe = value;
+                NotifyPropertyChanged("Heroe");
+            }
+        }
+
+        private bool _villano;
+
+        public bool Villano
+        {
+            get { return _villano; }
+            set {
+                if(_villano != value) { 
+                    _villano = value;
+
+                    NotifyPropertyChanged("Villano");
+                }
+                if (value)
+                {
+                    Xmen = false;
+                    Vengador = false;
+                }
+            }
+        }
+
+        
+
+        public Superheroe()
+        {
+            Heroe = true;//Borrar de cs
+        }
+
+        public Superheroe(string nombre, string imagen, bool vengador, bool xmen, bool heroe, bool villano)
+        {
+            Nombre = nombre;
+            Imagen = imagen;
+            Vengador = vengador;
+            Xmen = xmen;
+            Heroe = heroe;
+            Villano = villano;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public static List<Superheroe> GetSamples()
+        {
+            List<Superheroe> ejemplos = new List<Superheroe>();
+
+            Superheroe ironman = new Superheroe("Ironman", @"https://sm.ign.com/ign_latam/screenshot/default/ybbpqktez5whedr0-1592031889_31aa.jpg", true, false, true, false);
+            Superheroe kingpin = new Superheroe("Kingpin", @"https://www.comicbasics.com/wp-content/uploads/2017/09/Kingpin.jpg", false, false, false, true);
+            Superheroe spiderman = new Superheroe("Spiderman", @"https://wipy.tv/wp-content/uploads/2019/08/destino-de-%E2%80%98Spider-Man%E2%80%99-en-los-Comics.jpg", true, true, true, false);
+
+            ejemplos.Add(ironman);
+            ejemplos.Add(kingpin);
+            ejemplos.Add(spiderman);
+
+            return ejemplos;
+        }
+    }
+}
